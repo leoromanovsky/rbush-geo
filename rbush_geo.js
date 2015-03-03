28e74ecf6b653afe1190717e603c35abdda10143
Leo Romanovsky <leoromanovsky@gmail.com>
2015-03-02 23:09:15 -0800

    Add more support.

--- rbush_geo.js
+++ rbush_geo.js
@@ -4,30 +4,67 @@
  https://github.com/leoromanovsky/rbush_geo
 
  Wrapper for https://github.com/mourner/rbush by Vladimir Agafonkin.
-*/
+ */
 
 (function () { 'use strict';
 
-function rbush_geo(maxEntries, format) {
-  if (!(this instanceof rbush_geo)) return new rbush_geo(maxEntries, format);
-}
+    function rbush_geo(maxEntries, format) {
+        this._rbush = new rbush(maxEntries, format);
+        console.log("rbush", this._rbush);
 
-rbush_geo.prototype = {
-  all: function () {
-    console.log("all");
-    return this._all(this.data, []);
-  },
+        if (!(this instanceof rbush_geo)) {
+            return new rbush_geo(maxEntries, format);
+        }
 
-  _all: function (node, result) {
-    console.log("_all");
-    return [];
-  }
-}
+        this.clear();
+    }
 
-// export as AMD/CommonJS module or global variable
-if (typeof define === 'function' && define.amd) define('rbush_geo', function() { return rbush_geo; });
-else if (typeof module !== 'undefined') module.exports = rbush_geo;
-else if (typeof self !== 'undefined') self.rbush_geo = rbush_geo;
-else window.rbush_geo = rbush_geo;
+    rbush_geo.prototype = {
+        all: function () {
+            console.log("all");
+            return this._rbush.all();
+        },
 
+        insert: function(lat, lng) {
+            console.log("add", lat, lng);
+            this._rbush.insert([lat, lng, lat, lng]);
+        },
+
+        /*
+            data = [
+                [lat, lng],
+                [lat, lng
+            ]
+         */
+        load: function(data) {
+            console.log("load", data);
+
+            var converted = [];
+            data.forEach(function(d) {
+               converted.push([d[0], d[1], d[0], d[1]]);
+            });
+
+            this._rbush.load(converted);
+        },
+
+        /*
+        Search inside a bounding box.
+         */
+        search: function(minX, minY, maxX, maxY) {
+            console.log("search", minX, minY, maxX, maxY);
+            return this._rbush.search([minX, minY, maxX, maxY]);
+        },
+
+        clear: function() {
+            console.log("clear", this._rbush);
+            this._rbush.clear();
+            return this;
+        }
+    }
+
+    // export as AMD/CommonJS module or global variable
+    if (typeof define === 'function' && define.amd) define('rbush_geo', function() { return rbush_geo; });
+    else if (typeof module !== 'undefined') module.exports = rbush_geo;
+    else if (typeof self !== 'undefined') self.rbush_geo = rbush_geo;
+    else window.rbush_geo = rbush_geo;
 })();
